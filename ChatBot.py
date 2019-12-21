f4606c4acbffe2f6f7d3f2d2c4421adeb1cb5fd0
Stas <stanislavalex@mail.ru>
2019-12-21 18:04:05 +0300

    Add files via upload

--- ChatBot.py
+++ ChatBot.py
@@ -4,19 +4,18 @@ import requests
 import datetime
 import time
 import telegram 
-import telegram
 
 load_dotenv()
-token = os.getenv("token")
-telegramBotToken = os.getenv("telegramBotToken")
-chat_id = os.getenv("chat_id")
+TOKEN = os.getenv("token")
+TELEGRAMBOTTOKEN = os.getenv("telegramBotToken")
+CHAT_ID = os.getenv("chat_id")
 LIST_OF_CHECKED_TASKS = "https://dvmn.org/api/long_polling/?timestamp={}"
-headers = {"Authorization": token}
-timestamp = int(time.time())
+headers = {"Authorization": TOKEN}
+timestamp = {"timestamp":int(time.time())}
 
 while True:
     try:
-        response = requests.get(LIST_OF_CHECKED_TASKS.format(timestamp), headers=headers, timeout=100)
+        response = requests.get(LIST_OF_CHECKED_TASKS, params=timestamp, headers=headers, timeout=100)
         response.raise_for_status()
         response_status = response.json()
         if response_status['status'] == 'timeout':
@@ -24,8 +23,8 @@ while True:
         else:
             message = ("Преподаватель проверил работу:" + "\n" + response_status['new_attempts'][0]['lesson_title']
             + "\n" + "Ссылка на задачу: " + "https://dvmn.org" + response_status['new_attempts'][0]['lesson_url'])
-            bot = telegram.Bot(token=telegramBotToken)    
-            bot.send_message(chat_id=chat_id, text=message)  
+            bot = telegram.Bot(token=TELEGRAMBOTTOKEN)    
+            bot.send_message(chat_id=CHAT_ID, text=message)  
             break  	
     except requests.exceptions.ReadTimeout:
         print("There are no answer from the server at this moment. Waiting a response, please wait.")
