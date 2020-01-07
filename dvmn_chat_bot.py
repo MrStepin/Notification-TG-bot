30076f1dabce025b63188e77ad14935926b8a060
Stas <stanislavalex@mail.ru>
2020-01-07 23:48:23 +0300

    Add files via upload

--- dvmn_chat_bot.py
+++ dvmn_chat_bot.py
@@ -9,27 +9,27 @@ load_dotenv()
 token = os.getenv("TOKEN")
 telegram_bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
 chat_id = os.getenv("CHAT_ID")
-LIST_OF_CHECKED_TASKS = "https://dvmn.org/api/long_polling/"
+DVMN_API = "https://dvmn.org/api/long_polling/"
 headers = {"Authorization": token}
-current_time = {"timestamp":int(time.time())}
+api_request_params = {"timestamp":int(time.time())}
 
 while True:
     try:
-        response = requests.get(LIST_OF_CHECKED_TASKS, params=current_time, headers=headers, timeout=100)
+        response = requests.get(DVMN_API, params=api_request_params, headers=headers, timeout=100)
         response.raise_for_status()
         response_status = response.json()
         if response_status['status'] == 'timeout':
             time.sleep(5)
-            current_time = {"timestamp":int(time.time())}
+            api_request_params = {"timestamp":response_status['timestamp_to_request']}
         else:
-            attributes_of_lesson = response_status['new_attempts'][0]
-            name_of_lesson = attributes_of_lesson['lesson_title']
-            lesson_url = attributes_of_lesson['lesson_url']
-            message = (f"Преподаватель проверил работу: \n {name_of_lesson} \n Ссылка на задачу: https://dvmn.org{lesson_url}")
+            lesson = response_status['new_attempts'][0]
+            lesson_name = lesson['lesson_title']
+            lesson_url = lesson['lesson_url']
+            message = (f"Преподаватель проверил работу: \n {lesson_name} \n Ссылка на задачу: https://dvmn.org{lesson_url}")
             bot = telegram.Bot(token=telegram_bot_token)    
             bot.send_message(chat_id=chat_id, text=message)
             time.sleep(5)
-            current_time = {"timestamp":int(time.time())}   	
+            api_request_params = {"timestamp":response_status['last_attempt_timestamp']}   	
     except requests.exceptions.ReadTimeout:
         print("There are no answer from the server at this moment. Waiting a response, please wait.")
         pass
