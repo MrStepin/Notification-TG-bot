398f36d05073187ac3fce6db46b11f9b73ff6abd
Stas <stanislavalex@mail.ru>
2020-04-05 22:07:17 +0300

    Add files via upload

--- dvmn_chat_bot.py
+++ dvmn_chat_bot.py
@@ -6,23 +6,27 @@ import time
 import telegram 
 import logging
 
+class MyLogsHandler(logging.Handler):
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        bot.send_message(chat_id=chat_id, text=log_entry)
+
+logger = logging.getLogger("Logger")
+logger.addHandler(MyLogsHandler())
+
         
 if __name__ == '__main__':         
 
     load_dotenv()
-    devman_token = os.environ["TOKEN"]
-    telegram_bot_token = os.environ["TELEGRAM_BOT_TOKEN"]
-    chat_id = os.environ["CHAT_ID"]
+    devman_token = os.environ["DEVMAN_TOKEN"]
+    chat_id = os.environ["TELEGRAM_CHAT_ID"]
     DVMN_API = "https://dvmn.org/api/long_polling/"
     headers = {"Authorization": devman_token}
     api_request_params = {"timestamp":int(time.time())}
+    telegram_bot_token = os.environ["TELEGRAM_BOT_TOKEN"]
 
-    class MyLogsHandler(logging.Handler):
-
-        def emit(self, record):
-            log_entry = self.format(record)
-            bot = telegram.Bot(token=telegram_bot_token) 
-            bot.send_message(chat_id=chat_id, text=log_entry)
+    bot = telegram.Bot(token=telegram_bot_token) 
 
     while True:
         try:
@@ -31,21 +35,17 @@ if __name__ == '__main__':
                 response.raise_for_status()
                 devman_server_response = response.json()
                 if devman_server_response['status'] == 'timeout':
-                    time.sleep(2)
-                    api_request_params = {"timestamp":devman_server_response['timestamp_to_request']}
+                    api_request_params = {"timestamp": devman_server_response['timestamp_to_request'] + 2}
             except requests.exceptions.ReadTimeout:
                 pass        
             else:
                 lesson = devman_server_response['new_attempts'][0]
                 lesson_name = lesson['lesson_title']
                 lesson_url = lesson['lesson_url']
-                message = f"Преподаватель проверил работу: \n {lesson_name} \n Ссылка на задачу: https://dvmn.org{lesson_url}"
-                bot = telegram.Bot(token=telegram_bot_token)    
+                message = f"Преподаватель проверил работу: \n {lesson_name} \n Ссылка на задачу: https://dvmn.org{lesson_url}"   
                 bot.send_message(chat_id=chat_id, text=message)
                 api_request_params = {"timestamp":devman_server_response['last_attempt_timestamp']}    
         except Exception:
-            logger = logging.getLogger("Logger")
-            logger.addHandler(MyLogsHandler())
             logger.exception(logger)
 
                 
